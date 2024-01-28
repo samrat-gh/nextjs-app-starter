23ee2ad44043f72955a591f13d9eebc398ba8196
samrat-gh <samratghimire32@gmail.com>
2024-01-28 17:34:15 +0545

    add media-query viewports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,19 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "832px",
+      // => @media (min-width: 832px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
     },
   },
   plugins: [],
