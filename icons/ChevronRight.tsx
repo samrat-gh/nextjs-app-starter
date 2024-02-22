06917fc6aaf660ffa4a7060f6229c4bbbb010ae1
samrat-gh <samratghimire32@gmail.com>
2024-02-22 22:06:19 +0545

    add icon : ChevronRight

---
+++ icons/ChevronRight.tsx
@@ -0,0 +1,20 @@
+import type { FC, SVGProps } from 'react';
+
+const ChevronRight: FC<SVGProps<SVGSVGElement>> = ({ ...props }) => (
+  <svg
+    viewBox='0 0 24 24'
+    width='24'
+    height='24'
+    stroke='currentColor'
+    strokeWidth='1.5'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    fill='none'
+    shapeRendering='geometricPrecision'
+    {...props}
+  >
+    <path d='M9 18l6-6-6-6' />
+  </svg>
+);
+
+export default ChevronRight;
